85433412fc662f0bd8668b6d8691eb5239b8bb12
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-28 00:23:18 +0530

    type issue fix and js to ts conversion done

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
+  reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com", "image.tmdb.org"],
+    domains: ['links.papareact.com', 'image.tmdb.org'],
   },
 };
